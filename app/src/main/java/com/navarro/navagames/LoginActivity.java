1bd57a2f98ae2ac947d1a8a176bcd16d3be06829
DiegoGustavoRamirez <diegormz111@gmail.com>
2018-06-11 21:18:48 -0500

    Add function to button and get texts from EditText objects

--- app/src/main/java/com/navarro/navagames/LoginActivity.java
+++ app/src/main/java/com/navarro/navagames/LoginActivity.java
@@ -2,12 +2,41 @@ package com.navarro.navagames;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+
+    //Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        /*btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(view -> login());*/
+
     }
+
+    /*private void login(){
+        EditText etUserName = findViewById(R.id.etUser);
+        EditText etPassword = findViewById(R.id.etPassword);
+
+        Log.d("Usuario", etUserName.getText().toString());
+        Log.d("Contrasenia", etPassword.getText().toString());
+    }*/
+
+    public void login2(View view){
+        EditText etUserName = findViewById(R.id.etUser);
+        EditText etPassword = findViewById(R.id.etPassword);
+
+        Log.d("Usuario", etUserName.getText().toString());
+        Log.d("Contrasenia", etPassword.getText().toString());
+    }
+
 }
