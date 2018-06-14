8482254ef5e0be44bb6ee56daafd21057c2a845b
DiegoGustavoRamirez <diegormz111@gmail.com>
2018-06-13 21:00:03 -0500

    Intent implemented

--- app/src/main/java/com/navarro/navagames/LoginActivity.java
+++ app/src/main/java/com/navarro/navagames/LoginActivity.java
@@ -1,42 +1,43 @@
 package com.navarro.navagames;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
-
-    //Button btnLogin;
+    TextView tvRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        /*btnLogin = findViewById(R.id.btnLogin);
+        tvRegister = findViewById(R.id.tvRegister);
 
-        btnLogin.setOnClickListener(view -> login());*/
+        tvRegister.setOnClickListener(view -> {
+            // move to other activity
+            Intent goToRegister = new Intent(LoginActivity.this, RegisterActivity.class);
+            startActivity(goToRegister);
+
+            // kill actual activity
+            LoginActivity.this.finish();
+        });
 
     }
 
-    /*private void login(){
+    public void login(View view){
         EditText etUserName = findViewById(R.id.etUser);
         EditText etPassword = findViewById(R.id.etPassword);
 
         Log.d("Usuario", etUserName.getText().toString());
         Log.d("Contrasenia", etPassword.getText().toString());
-    }*/
 
-    public void login2(View view){
-        EditText etUserName = findViewById(R.id.etUser);
-        EditText etPassword = findViewById(R.id.etPassword);
-
-        Log.d("Usuario", etUserName.getText().toString());
-        Log.d("Contrasenia", etPassword.getText().toString());
+        //TODO: create a intent to go to MainActivity
     }
 
 }
---
+++ app/src/main/java/com/navarro/navagames/RegisterActivity.java
@@ -0,0 +1,21 @@
+package com.navarro.navagames;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+
+    }
+
+    // TODO: create a function to REGISTER USER
+
+
+}
